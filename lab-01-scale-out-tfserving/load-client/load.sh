ea3c229af08ebf506937999f85ca85975ad502ce
jarokaz <jaroka@no-reply.com>
2020-09-09 14:45:20 +0000

    Updates

--- lab-01-scale-out-tfserving/load-client/load.sh
+++ lab-01-scale-out-tfserving/load-client/load.sh
@@ -1,8 +1,8 @@
 IPADDRESS=${1:-10.138.0.5}
 DURATION=${2:-1m}
-QPS=${3:-1}
-CONNECTIONS=${4:-1}
-CONCURRENCY=${5:-1}
+QPS=${3:-10}
+CONNECTIONS=${4:-5}
+CONCURRENCY=${5:-5}
 TIMEOUT=1000ms
 ./ghz  --insecure --protoset ./tfserving.protoset --call tensorflow.serving.PredictionService.Predict -D resnet_serving_request.json \
   --timeout $TIMEOUT \
