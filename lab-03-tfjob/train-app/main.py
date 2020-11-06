a1034d2c783088968f3f9aea3196ef2f4437e360
Jarek Kazmierczak <jarek.r.kaz@gmail.com>
2020-11-06 08:05:37 -0800

    Update main.py

--- lab-03-tfjob/train-app/main.py
+++ lab-03-tfjob/train-app/main.py
@@ -21,4 +21,4 @@ with strategy.scope():
   multi_worker_model = mnist.build_and_compile_cnn_model()
 
 
-multi_worker_model.fit(multi_worker_dataset, epochs=3, steps_per_epoch=70)
+multi_worker_model.fit(multi_worker_dataset, epochs=5, steps_per_epoch=200)
